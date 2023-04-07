26c0cdbf0015811c5c613b6cb28ec3da6a104060
Alejandro Fernandez <fernandezale007@gmail.com>
2023-04-07 13:38:07 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove()
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+const yourName = "Alejandrp";
+newHeader.textContent = `${yourName} is the champion`;
+
+document.body.appendChild(newHeader);
